b09b0ecd9dc5d7e43ecf5cf22816435b1f201ac6
NoSkillGuy <rsivapraveen001@gmail.com>
2017-07-01 02:45:42 +0530

    1. Extended tour_guide_tag to handle arrays and handle quotes 1. When ever shepherd expects a value without quotes, In shepherd.yml surrond the value with $.

--- app/helpers/tour_guide_helper.rb
+++ app/helpers/tour_guide_helper.rb
@@ -1,32 +1,36 @@
 module TourGuideHelper
   def tour_guide_tag(steps: nil, theme: 'shepherd-theme-dark', start: true)
-    steps = steps || YAML.load_file("#{Rails.root}/config/shepherd.yml")[controller_name].try(:[], action_name).try(:[], "steps")
-    javascript = "
-      $(window).load(function(){
-        var tour;
-        tour = new Shepherd.Tour({
-          defaults: {
-            classes: '#{theme}'
-          }
-        });
-      "
-      steps.to_a.each do |step|
-        name = step.delete('name')
-        javascript << "tour.addStep('#{name}', {"
-        options = []
-        step.each do |key, value|
-          options << "#{key}: '#{value}'"
-        end
-        javascript << "#{options.join(',')}});"
-      end
-
-      javascript << 'tour.start();' if start
-
-      javascript << '});'
-
-      # p session[:_tour]
-      # p steps
-      steps ? javascript_tag(javascript,type: 'text/javascript') : ''
+    all_steps = all_steps || YAML.load_file("#{Rails.root}/config/shepherd.yml")[controller_name].try(:[], action_name).try(:[], "steps")
+    # All the js goes into javascript variable and is used in javascript tag
+    javascript = ''
+    open_after_window_load = "$(window).load(function() {"
+    shepherd_init_js = "
+      var tour;
+      tour = new Shepherd.Tour({
+        defaults: {
+          classes: '#{theme}',
+          showCancelLink: true,
+          scrollTo: true
+        }
+      });
+    "
+    # Shepherd all will contain all the steps
+    shepherd_steps = ''
+    all_steps.each do |each_step|
+      name = each_step.delete('name')
+      shepherd_steps << "tour.addStep('#{name}', {"
+      options = []
+      # Shepherd expects hash in symbol value format
+      # For every step pushing into shepherd_steps_array in the required format
+      each_step.each { |key, value| options << "#{key}: #{(value.is_a? Array) ? value : "'#{value}'"}" }
+      shepherd_steps << "#{options.join(',')} });"
+    end
+    start_tour = 'tour.start();' if start
+    close_after_window_load = '});'
+    javascript = open_after_window_load + shepherd_init_js + shepherd_steps + start_tour + close_after_window_load
+    # whenever shepherd expects some value without quotes, To determine this from Yaml file, we are prepending and appending method name with $
+    javascript = javascript.gsub("$\"",'').gsub("\"$",'').gsub('=>',': ')
+    all_steps ? javascript_tag(javascript,type: 'text/javascript') : ''
   rescue
     ''
   end
